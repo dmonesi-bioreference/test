1461c959b4977f858e2a9da9edf29ce89cc0a111
Kevin Zolkiewicz <kevin@zolk.com>
2018-04-07 21:48:04 -0500

    Redo icons

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SVG from 'react-inlinesvg';
+
+import IconAdd from '../../icons/add.svg';
+import IconArrowdown from '../../icons/arrowdown.svg';
+import IconChat from '../../icons/chat.svg';
+import IconCheckmark from '../../icons/checkmark.svg';
+import IconClose from '../../icons/close.svg';
+import IconDate from '../../icons/date.svg';
+import IconEmail from '../../icons/email.svg';
+import IconError from '../../icons/error.svg';
+import IconExpand from '../../icons/expand.svg';
+import IconInfo from '../../icons/info.svg';
+import IconLock from '../../icons/lock.svg';
+import IconNoentry from '../../icons/noentry.svg';
+import IconOverflow from '../../icons/overflow.svg';
+import IconSearch from '../../icons/search.svg';
+import IconSignout from '../../icons/signout.svg';
+import IconTrash from '../../icons/trash.svg';
+import IconUpload from '../../icons/upload.svg';
 
 const Icon = ({
   name,
-}) => (
-  <div className="c-icon">
-    <SVG src={`/icons/${name}.svg`} />
-  </div>
-);
+}) => {
+  const icons = {
+    add: IconAdd,
+    arrow: IconArrowdown,
+    chat: IconChat,
+    checkmark: IconCheckmark,
+    close: IconClose,
+    date: IconDate,
+    email: IconEmail,
+    error: IconError,
+    expand: IconExpand,
+    info: IconInfo,
+    lock: IconLock,
+    noentry: IconNoentry,
+    overflow: IconOverflow,
+    search: IconSearch,
+    signout: IconSignout,
+    trash: IconTrash,
+    upload: IconUpload,
+  };
+
+  const IconComponent = icons[name];
+
+  return (
+    <div className="c-icon">
+      <IconComponent />
+    </div>
+  );
+};
 
 export const iconArray = [
   '--',
