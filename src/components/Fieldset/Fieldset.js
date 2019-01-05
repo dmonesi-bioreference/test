214e6443b3b11d750717f62b7fa09084dbd1cedb
Kevin Zolkiewicz <kevin@zolk.com>
2019-01-04 21:18:27 -0600

    Add disabled attribute to Fieldset

--- src/components/Fieldset/Fieldset.js
+++ src/components/Fieldset/Fieldset.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Message from '../Message';
 
 const Fieldset = ({
-  children, invalid, invalidMessage, label, required,
+  children, disabled, invalid, invalidMessage, label, required,
 }) => {
   const invalidClass = invalid ? '--has-errors' : '';
   const bemClass = `o-input${invalidClass}`;
 
   return (
-    <fieldset className={bemClass}>
+    <fieldset className={bemClass} disabled={disabled}>
       <legend>
         {label} {required && <span>*</span>}
       </legend>
@@ -21,6 +21,7 @@ const Fieldset = ({
 
 Fieldset.propTypes = {
   children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
   invalid: PropTypes.bool,
   invalidMessage: PropTypes.string,
   label: PropTypes.string.isRequired,
@@ -28,6 +29,7 @@ Fieldset.propTypes = {
 };
 
 Fieldset.defaultProps = {
+  disabled: false,
   invalid: false,
   invalidMessage: null,
   required: false,
