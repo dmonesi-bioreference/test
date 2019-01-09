703a1cd9957ea01fde9f1099edf1ad5a3033a997
Kevin Zolkiewicz <kevin@zolk.com>
2019-01-09 17:49:41 -0600

    Convert icon to styled-components

---
+++ src/components/Icon/Icon.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import t from '../GlobalStyle/settings/tokens';
+
+const IconStyled = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export default IconStyled;
