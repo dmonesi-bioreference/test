997a2706de2ed417a4ecce593057eafe1e938aa8
Kevin Zolkiewicz <kevin@zolk.com>
2019-01-21 21:13:57 -0600

    Update example integration app

--- example/integration-example/src/App.js
+++ example/integration-example/src/App.js
@@ -1,10 +1,19 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
-import { Button, InputText, Message, Card } from '@zolk/react-ui-starter-kit';
+import {
+  Button,
+  GlobalStyle,
+  InputText,
+  Message,
+  Card,
+} from '@zolk/react-ui-starter-kit';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Message icon="info">Please fill out this form to continue</Message>
         <Card title="Register">
           <InputText name="text" type="text" label="First Name" />
